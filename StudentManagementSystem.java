685c833b54eb51d035dc7f431c3ea6cd49ea4439
Abuakar Siddiq <abubakarsiddiq03480094247@gmail.com>
2025-12-08 00:01:57 +0500

    Updated student management system: added addStudent and viewStudents methods

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -37,15 +37,50 @@ public class StudentManagementSystem {
     }
 
     public static void showAdminPortal() {
-        System.out.println("\n===== ADMIN PORTAL =====");
-        System.out.println("1. Add Student");
-        System.out.println("2. View Students");
-        System.out.println("3. Add Course");
-        System.out.println("4. View Courses");
-        System.out.println("5. Enroll Student");
-        System.out.println("6. View Enrollments");
-        System.out.println("7. Logout");
-        System.out.println("--------------------------");
+        Scanner input = new Scanner(System.in);
+        int choice ;
+        do{
+           System.out.println("\n===== ADMIN PORTAL =====");
+           System.out.println("1. Add Student");
+           System.out.println("2. View Students");
+           System.out.println("3. Add Course");
+           System.out.println("4. View Courses");
+           System.out.println("5. Enroll Student");
+           System.out.println("6. View Enrollments");
+           System.out.println("7. Logout");
+           System.out.println("--------------------------");
+          choice = input.nextInt();
+          switch(choice){
+            case 1:
+                addstudent();
+                break;
+            case 2:
+                viewStudents();
+                break;
+
+            case 3:
+                addCourse();
+                break;
+            case 4:
+                viewCourses();
+                break;
+            case 5:
+                enrollStudent();
+                break;
+            case 6:
+                viewEnrollemnets();
+                break;
+            case 7:
+                System.out.println("Logging out...");  
+                break;
+            default:
+                System.out.println("Invalid choice  ! Try again.");   
+          }
+        
+
+     
+
+        }
     }
 
     public static void showStudentPortal() {
